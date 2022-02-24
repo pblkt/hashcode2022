2e81274fcd698065422e0a53757182dbdc5bb5a7
Ophir <ophir@medigate.io>
2022-02-24 20:09:34 +0200

    add typing

--- round_one/scorer.py
+++ round_one/scorer.py
@@ -1,5 +1,28 @@
-#!/usr/bin/env python3.8
-from typing import Any, Dict
+#!/usr/bin/env python3
+from typing import Any, Dict, NamedTuple, List, Set, Union
+
+
+class Assingment(NamedTuple):
+    name: str
+    devs: Set[str]
+
+class Devs(NamedTuple):
+    name: str
+    skills: Dict[str, int]
+
+
+class Requirement(NamedTuple):
+    skill: str
+    min_level: int
+
+
+class Projects(NamedTuple):
+    name: str
+    duration: int
+    best_before: int
+    score: int
+    roles: List[Requirement]
+
 
 
 def score_solution(input: Dict[str, Any], output: Dict[str, Any]) -> int:
@@ -10,5 +33,8 @@ def score_solution(input: Dict[str, Any], output: Dict[str, Any]) -> int:
     pass
 
 
-def is_valid(input: Dict[str, Any], output: Dict[str, Any]) -> bool:
+def is_valid(input: Dict[str, Union[Projects, Devs]], output: Dict[str, List[Assingment]]) -> bool:
     return True
+
+if __name__ == "__main__":
+    pass # score_solution(input, output)

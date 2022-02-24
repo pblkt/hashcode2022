07569a68b8d9a91f883a439492924ba3b32ba3f0
Ophir <ophir@medigate.io>
2022-02-24 20:33:44 +0200

    add validators

--- round_one/scorer.py
+++ round_one/scorer.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python3
 from typing import Any, Dict, NamedTuple, List, Set, Union
 
-
-class Assingment(NamedTuple):
-    name: str
-    devs: Set[str]
-
-class Devs(NamedTuple):
-    name: str
-    skills: Dict[str, int]
-
-
-class Requirement(NamedTuple):
-    skill: str
-    min_level: int
-
-
-class Projects(NamedTuple):
-    name: str
-    duration: int
-    best_before: int
-    score: int
-    roles: List[Requirement]
-
+from round_one.types import Project, Assignment, Dev
 
 
 def score_solution(input: Dict[str, Any], output: Dict[str, Any]) -> int:
@@ -33,8 +12,50 @@ def score_solution(input: Dict[str, Any], output: Dict[str, Any]) -> int:
     pass
 
 
-def is_valid(input: Dict[str, Union[Projects, Devs]], output: Dict[str, List[Assingment]]) -> bool:
+def validate_assignment(project: Project, devs: Dev) -> bool:
+    assert len(project.roles) == len(devs)
+
+    max_dev_skills = {}
+    for dev in devs:
+        for skill in dev.skills.keys():
+            if skill not in max_dev_skills:
+                max_dev_skills[skill] = dev.skills[skill]
+            else:
+                max_dev_skills[skill] = max(max_dev_skills[skill], dev.skills[skill])
+
+    for role, dev in zip(project.roles, devs):
+        assert role.min_level <= devs.skills.get(role.skill, 0) or (
+            (role.min_level == devs.skills.get(role.skill, 0) + 1)
+            and (role.skill in max_dev_skills and role.min_level <= max_dev_skills[role.skill])
+        )
+
     return True
 
+
+def is_valid(
+    input: Dict[str, Union[Project, Dev]], assignments: List[Assignment]
+) -> bool:
+    projects: Project = input["projects"]
+    devs: Dev = input["devs"]
+
+    # Sanity
+    output_project_names = [assignment.name for assignment in assignments]
+    # Unique project name in output
+    assert len(output_project_names) == len(set(output_project_names))
+    output_project_names = set(output_project_names)
+    output_dev_names = set([assignment.devs for assignment in assignments])
+
+    input_projects_names = set([project.name for project in projects])
+    input_dev_names = set([dev.name for dev in devs])
+    assert output_dev_names <= input_dev_names
+    assert output_project_names <= input_projects_names
+
+    # Unique dev in each project
+    for assignment in assignments:
+        assert len(set(assignment.devs)) == len(assignment.devs)
+
+    return True
+
+
 if __name__ == "__main__":
-    pass # score_solution(input, output)
+    pass  # score_solution(input, output)

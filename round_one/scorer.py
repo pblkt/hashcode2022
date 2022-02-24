b048599d4edb8a8470edb3b61abfe7d0c2692193
Ophir <ophir@medigate.io>
2022-02-24 22:55:38 +0200

    bugfix

--- round_one/scorer.py
+++ round_one/scorer.py
@@ -113,7 +113,8 @@ def is_valid(
 
     input_projects_names = set([project.name for project in projects])
     input_dev_names = set([dev.name for dev in devs])
-    assert output_dev_names <= input_dev_names, f"{output_dev_names=} {input_dev_names=}"
+    assert output_dev_names <= input_dev_names
+    assert input_projects_names <= output_project_names
 
     # Verify each dev appear only once
     for assignment in assignments:

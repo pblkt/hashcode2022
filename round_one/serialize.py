e1f3f6f66c0130c17b0c1744a2ff150d235918a7
Daniel Finkelshtein <danielf@medigate.io>
2022-02-24 21:23:40 +0200

    serialize

--- round_one/serialize.py
+++ round_one/serialize.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3.8
-from typing import Dict, Any
+from typing import List
+
+from our_types import Assignment
 
 
-def serialize(solution: Dict[str, Any]) -> str:
-    pass
+def serialize(solution: List[Assignment]) -> str:
+    result = [str(len(solution))]
+    for assignment in solution:
+        result.append(assignment.name)
+        result.append(" ".join(assignment.devs))
+
+    return "\n".join(result)
+
+
+TESTCASE = [
+    Assignment(name="WebServer", devs=["Bob", "Anna"]),
+    Assignment(name="Logging", devs=["Anna"]),
+    Assignment(name="WebChat", devs=["Maria", "Bob"]),
+]
+
+if "__main__" == __name__:
+    print(serialize(TESTCASE))
